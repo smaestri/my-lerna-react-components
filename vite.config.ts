af9e857ea8ad0d34cee50988e4e071938703d2fc
Sylvain Maestri <sylvainmaestri@gmail.com>
2025-03-22 22:25:37 +0100

    setu lerna

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "./lib/index.tsx"),
-      name: "my-components",
-      fileName: (format) => `index.${format}.js`
-    }, 
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDom"
-        }
-      }
-    }
-  },
-  plugins: [react()],
+  plugins: [react()], 
 })
